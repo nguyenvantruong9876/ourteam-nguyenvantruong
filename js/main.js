f4676bcef78c9ef3c8b498d089eac24649afd40f
nguyenvantruong9876 <nguyenvantruong9876@gmail.com>
2022-08-19 15:33:09 +0700

    upade

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    
+    if (scroll >280) {
+        $("#ourtean").addClass("nav-fixed");
+    } else {
+        $("ourtean").removeClass("nav-fixed");
+    }
+});
